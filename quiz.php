dff89424bccc6e0dc4a42b81bd95a4afccbad24f
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-03-21 15:23:40 +0900

    quiz view 編集

--- quiz.php
+++ quiz.php
@@ -1,18 +1,24 @@
 <?php include'inc/header.php' ?>
+<main>
     <div class="quiz_box">
         <div class="quiz">
-            <p>問題１</p>
+            <h2>問題１</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam tenetur</p>
         </div>
         <div class="choices">
-            <ul>
+            <ol>
                 <li>選択肢１</li>
                 <li>選択肢２</li>
                 <li>選択肢３</li>
                 <li>選択肢４</li>
-            </ul>
+            </ol>
         </div>
     </div>
+    <div class="answer_box">
+        <h3 class="answer">選択肢１</h3>
+        <p class="answer_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo quasi voluptatum nam vero iure corporis unde sed facilis, voluptatibus repellat amet, ipsam minus veniam eum fuga assumenda commodi quidem.</p>
+    </div>
+</main>
 <?php include'inc/footer.php' ?>
 
 </html>
