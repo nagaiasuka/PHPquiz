23dd62dc3866e307ecd1eb5ae8f8a598e3ac7190
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-03-27 16:06:29 +0900

    取り出したデータを表示

--- html/question.php
+++ html/question.php
@@ -5,26 +5,25 @@ require __DIR__.'/../lib/functions.php';
 $id = '3';
 
 $data = fetchById($id);
-var_dump($data);
 
-$question = 'Laorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam tenetur';
+$question = $data[1];
 
 
 $answers=[
-    "A" => "選択肢１",
-    "B" => "選択肢2",
-    "C" => "選択肢3",
-    "D" => "選択肢4",
+    "A" => $data[2],
+    "B" => $data[3],
+    "C" => $data[4],
+    "D" => $data[5],
 ];
 
 // 答え（ABCD）
-$answer="A";
+$answer=strtoupper($data[6]);
 
 // 答えの文字
 $answer_value=$answers[$answer];
 
 // 解説文
-$answer_text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo quasi voluptatum nam vero iure corporis unde sed facilis, voluptatibus repellat amet, ipsam minus veniam eum fuga assumenda commodi quidem.";
+$answer_text=$data[7];
 
 include __DIR__.'/../inc/quiz.php';
 ?>
